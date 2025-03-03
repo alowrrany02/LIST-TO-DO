5f3e1886f6dd1334187b1e3dbfbcc6a8fd398e5b
alowrrany02 <130370163+alowrrany02@users.noreply.github.com>
2025-03-03 13:49:03 -0300

    Up

--- List_to_do/src/componentes/TodoForm.jsx
+++ List_to_do/src/componentes/TodoForm.jsx
@@ -3,8 +3,18 @@ import React from 'react'
 const TodoForm = () => {
   return (
     <div>
-      <div >
-        <h1>Todofrom</h1>
+      <div className='todo-form' >
+        <h2>Criar Tarefa:</h2>
+        <form>
+            <input type="text" placeholder='Digite o título' />
+            <select>
+                <option value=""> Selecione uma Categoria</option>
+                <option value="Trabalho"> Selecione uma Categoria</option>
+                <option value="Pessoal"> Selecione uma Categoria</option>
+                <option value="Estudos"> Selecione uma Categoria</option>
+            </select>
+            <button type='submit'>Criar Tarefa</button>
+        </form>
       </div>
     </div>
   )
