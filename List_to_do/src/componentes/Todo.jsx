6e3cd504a3e0039cac9e299245a83e82f3d3040a
alowrrany02 <130370163+alowrrany02@users.noreply.github.com>
2025-04-28 17:13:18 -0300

    up

--- List_to_do/src/componentes/Todo.jsx
+++ List_to_do/src/componentes/Todo.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Todo = ({todo}) => {
+const Todo = ({todo, removeTodo, completeTodo}) => {
   return (
     <div>
-      <div className='todo'>
+      <div className='todo' style={{textDecoration: todo.isCompleted ? "line-through": ""}}>
             <div className='content'>
               <p>{todo.text}</p>
               <p className='category'>
@@ -11,8 +11,8 @@ const Todo = ({todo}) => {
               </p>
             </div>
             <div>
-              <button className='complete'>Completar</button>
-              <button className='remove'>x</button>
+              <button className='complete' onClick={() => completeTodo(todo.id)}>Completar</button>
+              <button className='remove' onClick={()=> removeTodo(todo.id)} >x</button>
             </div>
           </div>
     </div>
